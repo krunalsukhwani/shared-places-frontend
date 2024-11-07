98f4841d3e6b905bc4c8ba0c52fa76f3c152f610
krunalsukhwani <krunal.sukhwani@gmail.com>
2024-11-06 19:32:12 -0500

    Added Navigation Links

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -3,22 +3,24 @@ import { Link } from "react-router-dom";
 
 import "./MainNavigation.css";
 import MainHeader from "./MainHeader";
+import NavLinks from "./NavLinks";
 
-
-const MainNavigation = props => {
-    return <MainHeader>
-        <button className="main-navigation__menu-btn">
-            <span />
-            <span />
-            <span />
-        </button>
-        <h1 className="main-navigation__title">
-            <Link to="/">YourPlaces</Link>
-        </h1>
-        <nav>
-            ...
-        </nav>
+const MainNavigation = (props) => {
+  return (
+    <MainHeader>
+      <button className="main-navigation__menu-btn">
+        <span />
+        <span />
+        <span />
+      </button>
+      <h1 className="main-navigation__title">
+        <Link to="/">YourPlaces</Link>
+      </h1>
+      <nav>
+        <NavLinks />
+      </nav>
     </MainHeader>
+  );
 };
 
 export default MainNavigation;
