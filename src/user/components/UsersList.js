ffa584799aa294dc9439bf101c042115e1018302
krunalsukhwani <krunal.sukhwani@gmail.com>
2024-11-06 19:02:16 -0500

    Added Card Component

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -2,13 +2,16 @@ import React from "react";
 
 import "./UsersList.css";
 import UserItem from "./UserItem";
+import Card from "../../shared/components/UIElements/Card";
 
 const UsersList = (props) => {
   //if users are not available in the array
   if (props.items.length === 0) {
     return (
       <div className="center">
-        <h2>No Users Found.</h2>
+        <Card>
+          <h2>No Users Found.</h2>
+        </Card>
       </div>
     );
   }
